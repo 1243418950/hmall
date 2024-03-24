7e37f4f428b7aa4657fbc51f96dff1afc8b77871
MarkXu <1243418950@qq.com>
2024-03-24 13:28:18 +0800

    openfeign微服务间传递

---
+++ hm-api/src/main/java/com/hmall/api/config/DefaultFeignConfig.java
@@ -0,0 +1,28 @@
+package com.hmall.api.config;
+
+import com.hmall.common.utils.UserContext;
+import feign.Logger;
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
+import org.springframework.context.annotation.Bean;
+
+public class DefaultFeignConfig {
+    @Bean
+    public Logger.Level feignLoggerLevel(){
+        return Logger.Level.FULL;
+    }
+
+    @Bean
+    public RequestInterceptor userInfoRequestInterceptor(){
+        return new RequestInterceptor() {
+            @Override
+            public void apply(RequestTemplate requestTemplate) {
+                Long user = UserContext.getUser();
+                if(user!=null){
+                    requestTemplate.header("user-info", user.toString());
+                }
+
+            }
+        };
+    }
+}
